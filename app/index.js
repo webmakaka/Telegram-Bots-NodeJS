0b46d63aa02fc780e84ae3b91ed453f57efad9e1
Marley <marley.nodejs@gmail.com>
2023-06-02 18:27:44 +0300

    Создание Telegram бота на Node.js / #9 - Обработка команд

--- app/index.js
+++ app/index.js
@@ -14,22 +14,32 @@ const bot = new TelegramBot(TOKEN, {
   },
 });
 
-bot.on('message', (msg) => {
+// bot.on('message', (msg) => {
+//   const { id } = msg.chat;
+
+//   if (msg.text.toLowerCase() === 'hello') {
+//     bot.sendMessage(id, `Hello, ${msg.from.first_name}`);
+//   } else {
+//     bot.sendMessage(id, 'Здравствуй, ' + msg.from.first_name);
+//     bot.sendMessage(id, debug(msg));
+//   }
+
+//   bot
+//     .sendMessage(id, debug(msg))
+//     .then(() => {
+//       console.log('Message has been sent');
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// });
+
+bot.onText(/\/start/, (msg) => {
   const { id } = msg.chat;
+  bot.sendMessage(id, debug(msg));
+});
 
-  if (msg.text.toLowerCase() === 'hello') {
-    bot.sendMessage(id, `Hello, ${msg.from.first_name}`);
-  } else {
-    bot.sendMessage(id, 'Здравствуй, ' + msg.from.first_name);
-    bot.sendMessage(id, debug(msg));
-  }
-
-  bot
-    .sendMessage(id, debug(msg))
-    .then(() => {
-      console.log('Message has been sent');
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+bot.onText(/\/help (.+)/, (msg, [source, match]) => {
+  const { id } = msg.chat;
+  bot.sendMessage(id, debug(match));
 });

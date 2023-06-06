0a3b1c50ac20347b5ba7d6a7d4f89d380f16a663
Marley <marley.nodejs@gmail.com>
2023-06-06 17:25:06 +0300

    18 - Редактирование сообщения

--- app/index.js
+++ app/index.js
@@ -239,6 +239,48 @@ const bot = new TelegramBot(TOKEN, {
 
 // 17 - Ответ на сообщение
 
+// const inline_keyboard = [
+//   [
+//     { text: 'Forward', callback_data: 'forward' },
+//     { text: 'Reply', callback_data: 'reply' },
+//   ],
+//   [
+//     { text: 'Edit', callback_data: 'edit' },
+//     { text: 'Delete', callback_data: 'delete' },
+//   ],
+// ];
+
+// bot.on('callback_query', (query) => {
+//   const { chat, message_id, text } = query.message;
+
+//   switch (query.data) {
+//     case 'forward':
+//       // куда, откуда, что
+//       bot.forwardMessage(chat.id, chat.id, message_id);
+//       break;
+//     case 'reply':
+//       bot.sendMessage(chat.id, `Отвечаем на сообщение`, {
+//         reply_to_message_id: message_id,
+//       });
+//   }
+
+//   bot.answerCallbackQuery({
+//     callback_query_id: query.id,
+//   });
+// });
+
+// bot.onText(/\/start/, (msg, [source, match]) => {
+//   const chatId = msg.chat.id;
+
+//   bot.sendMessage(chatId, 'keyboard', {
+//     reply_markup: {
+//       inline_keyboard,
+//     },
+//   });
+// });
+
+// 18 - Редактирование сообщения
+
 const inline_keyboard = [
   [
     { text: 'Forward', callback_data: 'forward' },
@@ -261,6 +303,12 @@ bot.on('callback_query', (query) => {
     case 'reply':
       bot.sendMessage(chat.id, `Отвечаем на сообщение`, {
         reply_to_message_id: message_id,
+      });
+    case 'edit':
+      bot.editMessageText(`${text} (edited)`, {
+        chat_id: chat.id,
+        message_id,
+        reply_markup: { inline_keyboard },
       });
   }
 

cad4a1b961ae900d7c61c57d8b4cf4386e1b9acf
Marley <marley.nodejs@gmail.com>
2023-06-06 17:33:09 +0300

    19 - Удаление сообщения

--- app/index.js
+++ app/index.js
@@ -262,6 +262,7 @@ const bot = new TelegramBot(TOKEN, {
 //       bot.sendMessage(chat.id, `Отвечаем на сообщение`, {
 //         reply_to_message_id: message_id,
 //       });
+//      break;
 //   }
 
 //   bot.answerCallbackQuery({
@@ -280,6 +281,56 @@ const bot = new TelegramBot(TOKEN, {
 // });
 
 // 18 - Редактирование сообщения
+
+// const inline_keyboard = [
+//   [
+//     { text: 'Forward', callback_data: 'forward' },
+//     { text: 'Reply', callback_data: 'reply' },
+//   ],
+//   [
+//     { text: 'Edit', callback_data: 'edit' },
+//     { text: 'Delete', callback_data: 'delete' },
+//   ],
+// ];
+
+// bot.on('callback_query', (query) => {
+//   const { chat, message_id, text } = query.message;
+
+//   switch (query.data) {
+//     case 'forward':
+//       // куда, откуда, что
+//       bot.forwardMessage(chat.id, chat.id, message_id);
+//       break;
+//     case 'reply':
+//       bot.sendMessage(chat.id, `Отвечаем на сообщение`, {
+//         reply_to_message_id: message_id,
+//       });
+//      break;
+//     case 'edit':
+//       bot.editMessageText(`${text} (edited)`, {
+//         chat_id: chat.id,
+//         message_id,
+//         reply_markup: { inline_keyboard },
+//       });
+//      break;
+//   }
+
+//   bot.answerCallbackQuery({
+//     callback_query_id: query.id,
+//   });
+// });
+
+// bot.onText(/\/start/, (msg, [source, match]) => {
+//   const chatId = msg.chat.id;
+
+//   bot.sendMessage(chatId, 'keyboard', {
+//     reply_markup: {
+//       inline_keyboard,
+//     },
+//   });
+// });
+
+// 19 - Удаление сообщенияs
 
 const inline_keyboard = [
   [
@@ -304,12 +355,17 @@ bot.on('callback_query', (query) => {
       bot.sendMessage(chat.id, `Отвечаем на сообщение`, {
         reply_to_message_id: message_id,
       });
+      break;
     case 'edit':
       bot.editMessageText(`${text} (edited)`, {
         chat_id: chat.id,
         message_id,
         reply_markup: { inline_keyboard },
       });
+      break;
+    case 'delete':
+      bot.deleteMessage(chat.id, message_id);
+      break;
   }
 
   bot.answerCallbackQuery({
